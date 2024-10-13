34728f4d0c01ba1967b98b4d81cb47b5b3db4d9f
Sébastien Anthamatten <>
2024-10-13 21:01:45 +0200

    Various changes and updates: - Refactoring - Artifact-github repo mapping - Utilities

--- Scraper/Program.cs
+++ Scraper/Program.cs
@@ -1,5 +1,6 @@
 ﻿namespace Scraper;
 
+using System.ComponentModel;
 using System.Net;
 using System.Runtime.InteropServices;
 using System.Text;
@@ -9,6 +10,7 @@ using AngleSharp.Html.Parser;
 
 class Program
 {
+    public static readonly string ARTIFACT_LINK_PATH = "/artifact/";
     static async Task Main(string[] args)
     {
         var mvnRepositoryBaseUrl = "http://mvnrepository.com";
@@ -16,35 +18,48 @@ class Program
         var searchQuery = "java%20library%20github";
         // var searchQuery = "PhilJay/MPAndroidChart";
         // var searchQuery = "library";
+        // var categorySearch = "open-source/testing-frameworks"; //done
+        // var categorySearch = "open-source/config-libraries"; //done
+        // var categorySearch = "open-source/concurrency-libraries"; // done
+        var categorySearch = "open-source/reflection-libraries"; //done
+        // var categorySearch = "open-source/assertion-libraries"; //done
+        // var categorySearch = "open-source/validation"; //done
+        // var categorySearch = "open-source/bytecode-libraries"; //done
+        // var categorySearch = "open-source/base64-libraries"; //done
+        // var categorySearch = "open-source/json-libraries"; //done
+        // var categorySearch = "open-source/annotation-libraries";
+
         var sortOption = "popular";
         var minPageNb = 1;
-        var maxPageNb = 1;
+        var maxPageNb = 10;
 
         var _program = new Program();
 
         // We will store the html response of the request here
         var siteContent = string.Empty;
 
-        var now = DateTime.Now.Ticks;
+        long unixNow = ((DateTimeOffset)DateTime.Now).ToUnixTimeSeconds();
         var outputDirPath = "../../APIDiff/dataset/ToProcess/";
         var outputDirUsagesPath = outputDirPath + "Usages/";
         var exceptionsDirPath = "../../APIDiff/dataset/Exceptions/";
         //var fileName = $"Results/scrap_p{minPageNb}_{maxPageNb}_{now}.csv";
         var csv = ".csv";
-        var fileName = $"{now}_scrap_p{minPageNb}_{maxPageNb}";
-        var exLog = $"{exceptionsDirPath}{now}_exceptions";
+        var fileName = $"{unixNow}_scrap_p{minPageNb}_{maxPageNb}";
+        var exLog = $"{exceptionsDirPath}{unixNow}_exceptions";
         var builder = new StringBuilder();
         var exceptionsLog = new StringBuilder();
 
         var mvnRepositorySearchOptions = string.Empty;
 
-        await _program.ProcessQuery(minPageNb, maxPageNb, mvnRepositorySearchOptions, searchQuery, sortOption,
-                                    mvnRepositoryBaseUrl, mvnCentralBaseUrl, exceptionsLog, exLog, builder, fileName,
-                                    outputDirPath, _program, now, csv, false);
+        await _program.ProcessQuery(minPageNb, maxPageNb, mvnRepositorySearchOptions,
+        searchQuery, categorySearch,
+        sortOption, mvnRepositoryBaseUrl, mvnCentralBaseUrl, exceptionsLog, exLog, builder, fileName,
+                                    outputDirPath, _program, unixNow, csv, false);
 
     }
 
-    private async Task ProcessQuery(int minPageNb, int maxPageNb, string mvnRepositorySearchOptions, string searchQuery,
+    private async Task ProcessQuery(int minPageNb, int maxPageNb, string mvnRepositorySearchOptions,
+                                    string searchQuery, string categorySearch,
                                     string sortOption, string mvnRepositoryBaseUrl, string mvnCentralBaseUrl,
                                     StringBuilder exceptionsLog, string exLog, StringBuilder builder, string fileName,
                                     string outputDirPath,
@@ -56,7 +71,12 @@ class Program
 
             if (!isUsageQuery)
             {
-                mvnRepositorySearchOptions = $"/search?q={searchQuery}&p={curPage}&sort={sortOption}";
+                // mvnRepositorySearchOptions = $"/search?q={searchQuery}&p={curPage}&sort={sortOption}";
+                // Hardcoded list of categories ? 
+                // var categorySearch = "open-source/testing-frameworks";
+                // mvnRepositorySearchOptions = $"/{categorySearch}?p={curPage}&sort={sortOption}";
+                mvnRepositorySearchOptions = $"/{categorySearch}?p={curPage}";
+
                 searchUrl = mvnRepositoryBaseUrl + mvnRepositorySearchOptions;
             }
             else
@@ -91,6 +111,8 @@ class Program
             var headerLine = "ArtifactTitle;ArtifactLink;Title;GithubLinks";
             builder.AppendLine(headerLine);
 
+            var atLeastOneLine = false;
+
             foreach (var artifactLink in artifactLinks)
             {
                 var mvnDetailPageContent = await _program.GetContentAsync(mvnRepositoryBaseUrl + artifactLink, exceptionsLog);
@@ -103,6 +125,7 @@ class Program
                 var title = titleNode?.TextContent;
                 var artifactTitleNode = centralDetailPageDocument?.QuerySelector("h1");
                 var artifactTitle = artifactTitleNode?.TextContent;
+                var fileNameArtifactWithOrg = convertArtifactPathToFilename(artifactLink);
                 //var githubLinkNodes = centralDetailPageDocument?.QuerySelectorAll("a").Where(n => n.Attributes["href"]?.Text().Contains("github") ?? false);
                 var githubLinkNodes = centralDetailPageDocument?.All.Where(n => n.LocalName == "a" && (n.Attributes["href"]?.Text().Contains("github") ?? false) && n.Children.Any(cn => cn.TextContent == "Source Control"));
                 // var githubLinks = string.Join(',', githubLinkNodes?.Select(n => " " + n.Attributes["href"]?.TextContent) ?? new List<string>());
@@ -130,20 +153,27 @@ class Program
                     var lineToAdd = $"{artifactTitle}; {artifactLink}; {title}; {validGitRepoLinksString}";
                     Console.WriteLine($"Page: {curPage}, line: {lineToAdd}");
                     builder.AppendLine(lineToAdd);
+                    if (!atLeastOneLine)
+                    {
+                        atLeastOneLine = true;
+                    }
                 }
 
                 if (!isUsageQuery)
                 {
-                    await ProcessQuery(0, 0, mvnRepositoryBaseUrl + artifactLink + "/usages", string.Empty, string.Empty, mvnRepositoryBaseUrl,
-                                       mvnCentralBaseUrl, new StringBuilder(), $"{exLog}_{artifactTitle}_usages",
-                                       new StringBuilder(), $"{fileName}_{artifactTitle}_usages",
+                    await ProcessQuery(0, 0, mvnRepositoryBaseUrl + artifactLink + "/usages", string.Empty, string.Empty, string.Empty,
+                                       mvnRepositoryBaseUrl, mvnCentralBaseUrl, new StringBuilder(), $"{exLog}_{fileNameArtifactWithOrg}_usages",
+                                       new StringBuilder(), $"{fileName}_{fileNameArtifactWithOrg}_usages",
                                        outputDirPath + "Usages/", _program, now, csv, true);
                 }
             }
 
             try
             {
-                File.WriteAllText(outputDirPath + fileName + csv, builder.ToString());
+                if (atLeastOneLine)
+                {
+                    File.WriteAllText(outputDirPath + fileName + csv, builder.ToString());
+                }
                 File.WriteAllText(exLog + csv, exceptionsLog.ToString());
             }
             catch (Exception ex)
@@ -205,5 +235,12 @@ class Program
         }
 
         return content;
+    }
+
+    private static string convertArtifactPathToFilename(string artifactPath)
+    {
+        string converted = artifactPath.Trim().Replace(ARTIFACT_LINK_PATH, "").Replace("/", "-");
+
+        return converted;
     }
 }
